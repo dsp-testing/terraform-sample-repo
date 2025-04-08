e8e511986f70038ddbfffe48946e631f50176fac
Hariharan Thavachelvam <164553783+thavaahariharangit@users.noreply.github.com>
2025-04-08 14:38:22 +0000

    Add terraform file

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
-module "example" {
-  source = "./modules/example-module"
-
-  variable_name = "value"
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 5.0, != 5.86.0"
+      source  = "hashicorp/aws"
+    }
+    http = {
+      version = "~> 3.0"
+      source  = "hashicorp/http"
+    }
+    github = {
+      version = "~> 4.28.0"
+      source  = "integrations/github"
+    }
+    random = {
+      version = "~> 3.6"
+      source  = "hashicorp/random"
+    }
+  }
+  required_version = "~> 1.10"
 }
